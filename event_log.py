176aa75380b304201637b0bc83df901c1ea327a7
Sushma <113226796+SushMallesh@users.noreply.github.com>
2022-12-02 12:39:40 +0530

    Update event_log.py

--- event_log.py
+++ event_log.py
@@ -1,26 +1,33 @@
 import pandas as pd 
-import datetime
 
 log_file = pd.read_csv('eventlogs.csv')
 
 df = pd.DataFrame(log_file)
+
 # Total number of events 
 total_events = df.count()
+
 print('Total number of events: ',total_events[0])
+
 print('................................')
 # Number of events by Task Category
+
 count = df.groupby('Task Category').count()
 events_by_category = count.iloc[:,0]
+
 print(events_by_category)
+
 print('....................................')
 # logging start and end time 
 # end_event_time = df.head(1)['Date and Time']
 # start_event_time = df.tail(1)['Date and Time']
 # Event times 
 df = df.sort_values('Date and Time')
-df = df.loc[:,['Level','Event ID','Date and Time','Source','Task Category']]
-# showing 10 events here
+df = df.loc[:,['Event ID','Date and Time']]
+# showing 10 events here with time
 df = df.head(10)
 print(df)
-df = df.to_csv('modified.csv')
+
+# saving the result into another csv file
+df = df.to_csv('result.csv')
 
